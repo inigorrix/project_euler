369956b28ee7b28c708f656abaa6488c9ccd0653
inigorrix <inigorrix@gmail.com>
2025-01-02 01:41:36 +0100

    changed variable name

--- problem_002.jl
+++ problem_002.jl
@@ -1,15 +1,15 @@
 function sum_even_fibo(x)
     a, b, c = 1, 1, 2
-    sum = 0
+    total = 0
     while c<x
         #println(c)
-        sum += c
+        total += c
 
         a = b + c
         b = c + a
         c = a + b
     end
-    return sum
+    return total
 end
 
 println(sum_even_fibo(4e6))
